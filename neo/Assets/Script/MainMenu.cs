8bd1ecdbd75423b004a9cdce5f429fc7fde8ea86
apoelara23 <37701081+apoelara23@users.noreply.github.com>
2018-04-20 13:22:47 +0100

    removing zip -adding folder

---
+++ neo/Assets/Script/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+
+
+	public void QuitGame ()
+	{
+		Debug.Log ("Quit!!");
+		Application.Quit ();
+
+	}
+
+
+
+
+
+
+
+}
